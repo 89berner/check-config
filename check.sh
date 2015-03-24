a245743531b9ecc8030bba1f991e33385eb87b14
89berner <89berner@gmail.com>
2015-03-23 23:33:57 -0300

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sleep $(( RANDOM%600+1 ))
-dpkg -l > /tmp/dpkg.tmp && curl --connect-timeout 120 -F "data=@/tmp/dpkg.tmp"  http://127.0.0.1/pcheck/upload && rm /tmp/dpkg.tmp
-curl --connect-timeout 120 -F "data=@/etc/sudoers" http://127.0.0.1/pcheck/upload
-chkconfig --list > /tmp/chkconfig.tmp && curl --connect-timeout 120 -F "data=@/tmp/chkconfig.tmp"  http://127.0.0.1/pcheck/upload && rm /tmp/chkconfig.tmp
+dpkg -l > /tmp/dpkg.tmp && curl --connect-timeout 120 -F "data=@/tmp/dpkg.tmp"  http://127.0.0.1:4567/pcheck/upload && rm /tmp/dpkg.tmp
+curl --connect-timeout 120 -F "data=@/etc/sudoers" http://127.0.0.1:4567/pcheck/upload
+chkconfig --list > /tmp/chkconfig.tmp && curl --connect-timeout 120 -F "data=@/tmp/chkconfig.tmp"  http://127.0.0.1:4567/pcheck/upload && rm /tmp/chkconfig.tmp
 
